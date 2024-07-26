fbaab9e9bc8664d0e45aa4d78af470ce6127b6c0
Ypzar John <ypzarjohn24@gmail.com>
2024-07-26 09:10:45 +0800

    Fixing the route of the invoice order id

--- frontend/src/components/order/OrderDetails.jsx
+++ frontend/src/components/order/OrderDetails.jsx
@@ -37,9 +37,12 @@ const OrderDetails = () => {
         <div className="col-12 col-lg-9 mt-5 order-details">
           <div className="d-flex justify-content-between align-items-center">
             <h3 className="mt-5 mb-4">Your Order Details</h3>
-            <a className="btn btn-success" href="/invoice/order/order-id">
+            <Link
+              className="btn btn-success"
+              to={`/invoice/order/${order?._id}`}
+            >
               <i className="fa fa-print"></i> Invoice
-            </a>
+            </Link>
           </div>
           <table className="table table-striped table-bordered">
             <tbody>
